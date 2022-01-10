3db58afabb75d09028c0279c5a044f807bfde404
chamisarobert <chamisa.robert@gmail.com>
2022-01-10 17:14:47 +0200

    Added Basic Authentication to the API

--- nsApp/views.py
+++ nsApp/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from nsApp.serializers import AuthorSerializer, BookSerializer
 from nsApp.models import Author, Book
 from rest_framework import generics
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class AuthorListView(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class AuthorDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Author.objects.all()
